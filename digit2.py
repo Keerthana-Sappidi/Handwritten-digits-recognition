57b6c360339dc252703a4202b1a2af959f6c28ef
Keerthana-Sappidi <157403085+Keerthana-Sappidi@users.noreply.github.com>
2024-03-25 13:53:29 +0530

    Add files via upload

---
+++ digit2.py
@@ -0,0 +1,12 @@
+import cv2
+
+# Load an image
+img = cv2.imread(r'C:\Users\supriya\OneDrive\Desktop\digit2.png')
+
+# Create a named window
+cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
+
+# Display the image
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
